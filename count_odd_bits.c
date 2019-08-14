29470ebc3a59411214fcb2ea42f4b1bf61d30016
ohasi <ohadsirkis@gmail.com>
2019-08-14 15:07:05 +0300

    small fox

--- count_odd_bits.c
+++ count_odd_bits.c
@@ -5,7 +5,7 @@ int count_odd_bits(unsigned long);
 int main() {
 	unsigned long x = 0; /*the number to count odd on bits in*/
 	unsigned long result = 0; /*will hold the amount of odd unsigned bits*/ 
-	
+	printf("Please enter a number to count the amount of on bits in odd indexes in:\n");
 	scanf("%lu", &x); /*gets the number to count odd on bits in*/
 	result = count_odd_bits(x); /*gets the result*/
 	printf("The number of on bits in odd indexes in your number is: %lu\n", result); /*prints the result*/
